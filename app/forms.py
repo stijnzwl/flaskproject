6d75f8440232b2d0aade1daab4e171fa99f228a9
stijnzwl <stijnwillems97@hotmail.com>
2024-02-11 12:59:45 +0100

    added imports to forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
