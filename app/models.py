082c3e594c318648487d931e30debe43b49a5617
stijnzwl <stijnwillems97@hotmail.com>
2024-02-11 15:39:01 +0100

    added about me & last seen to User

--- app/models.py
+++ app/models.py
@@ -9,14 +9,18 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
+    
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+    
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
