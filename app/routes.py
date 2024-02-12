23a3e6d0908d0e50cd081f54b474b753d6b8b6c8
stijnzwl <stijnwillems97@gmail.com>
2024-02-12 20:46:33 +0100

    Explore page logic

--- app/routes.py
+++ app/routes.py
@@ -136,3 +136,11 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+
+@app.route('/explore')
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
+    return render_template('index.html', title='Explore', posts=posts)
