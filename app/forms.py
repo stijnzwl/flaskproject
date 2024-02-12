671da4f1d8fd2e3d2cd6965eaeba37a50749cf39
stijnzwl <stijnwillems97@gmail.com>
2024-02-12 19:38:00 +0100

    typo

--- app/forms.py
+++ app/forms.py
@@ -45,5 +45,5 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError('Please use a different username.')
             
-class EmptyForm(Flaskform):
+class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
