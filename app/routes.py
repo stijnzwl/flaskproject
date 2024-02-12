9722c9993ff1e1f08d2491e188a875d744e7dafc
stijnzwl <stijnwillems97@gmail.com>
2024-02-12 19:35:59 +0100

    added the (un)follow buttons to user.html

--- app/routes.py
+++ app/routes.py
@@ -73,7 +73,8 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @app.before_request
