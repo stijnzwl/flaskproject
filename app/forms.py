a4ab027ae5e4ec84a3d3a811690a810729a138bd
stijnzwl <stijnwillems97@gmail.com>
2024-02-12 15:55:39 +0100

    indentation fix

--- app/forms.py
+++ app/forms.py
@@ -39,8 +39,8 @@ class EditProfileForm(FlaskForm):
         super().__init__(*args, **kwargs)
         self.original_username = original_username
 
-        def validate_username(self, username):
-            if username.data != self.original_username:
-                user = db.session.scalar(sa.select(User).where(User.username) == self.username.data)
-                if user is not None:
-                    raise ValidationError('Please use a different username.')
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(User.username) == self.username.data)
+            if user is not None:
+                raise ValidationError('Please use a different username.')
