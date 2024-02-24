14e82149372869d05eedff5cf90025f6c29b2174
stijnzwl <stijnwillems97@gmail.com>
2024-02-24 16:50:16 +0100

    bugfix

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -19,6 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from flask_babel import _
 from threading import Thread
 
 
@@ -19,10 +20,9 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email(
-        "[Flaskproject] Reset Your Password",
+    send_email(_(
+        "[Flaskproject] Reset Your Password"),
         sender=app.config["ADMINS"][0],
         recipients=[user.email],
         text_body=render_template("email/reset_password.txt", user=user, token=token),
-        html_body=render_template("email/reset_password.html", user=user, token=token),
-    )
+        html_body=render_template("email/reset_password.html", user=user, token=token))
