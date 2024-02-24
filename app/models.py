2d60a60eec75345f673de30fb89958a4ed7d958d
stijnzwl <stijnwillems97@hotmail.com>
2024-02-24 21:36:09 +0100

    more translation stuff

--- app/models.py
+++ app/models.py
@@ -117,6 +117,7 @@ class Post(db.Model):
     )
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     author: so.Mapped[User] = so.relationship(back_populates="posts")
+    language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))
 
     def __repr__(self):
         return f"<Post {self.body}>"
