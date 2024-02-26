ac23bbd157b445200ca6ba1be848cfe53b035c82
stijnzwl <stijnwillems97@hotmail.com>
2024-02-26 20:05:50 +0100

    file structuring

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
-from hashlib import md5
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
+from hashlib import md5
+from time import time
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login, app
+from flask import current_app
 from flask_login import UserMixin
-from time import time
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import db, login
+
 
 followers = sa.Table(
     "followers",
@@ -22,11 +24,27 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc)
     )
+
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates="followers",
+    )
+    followers: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following",
+    )
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -37,20 +55,6 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
-
-    following: so.WriteOnlyMapped["User"] = so.relationship(
-        secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        back_populates="followers",
-    )
-
-    followers: so.WriteOnlyMapped["User"] = so.relationship(
-        secondary=followers,
-        primaryjoin=(followers.c.followed_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
-        back_populates="following",
-    )
 
     def follow(self, user):
         if not self.is_following(user):
@@ -83,7 +87,12 @@ class User(UserMixin, db.Model):
             sa.select(Post)
             .join(Post.author.of_type(Author))
             .join(Author.followers.of_type(Follower), isouter=True)
-            .where(sa.or_(Follower.id == self.id, Author.id == self.id))
+            .where(
+                sa.or_(
+                    Follower.id == self.id,
+                    Author.id == self.id,
+                )
+            )
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
@@ -91,22 +100,24 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {"reset_password": self.id, "exp": time() + expires_in},
-            app.config["SECRET_KEY"],
+            current_app.config["SECRET_KEY"],
             algorithm="HS256",
         )
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
-                "reset_password"
-            ]
-        except:
+            id = jwt.decode(
+                token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
+            )["reset_password"]
+        except Exception:
             return
         return db.session.get(User, id)
 
-    def __repr__(self):
-        return f"<User {self.username}>"
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 class Post(db.Model):
@@ -116,13 +127,9 @@ class Post(db.Model):
         index=True, default=lambda: datetime.now(timezone.utc)
     )
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    author: so.Mapped[User] = so.relationship(back_populates="posts")
     language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))
 
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
     def __repr__(self):
-        return f"<Post {self.body}>"
-
-
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
+        return "<Post {}>".format(self.body)
