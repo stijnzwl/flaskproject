20bf4327caa73bf78bc55223b53f809f43ae4ca7
stijnzwl <stijnwillems97@hotmail.com>
2024-02-29 18:25:03 +0100

    some additions

--- app/casino/blackjack.py
+++ app/casino/blackjack.py
@@ -1,5 +1,5 @@
 from random import randint, shuffle
-
+from flask import url_for
 
 
 class Blackjack:
@@ -17,60 +17,47 @@ class Blackjack:
         ]
 
     def create_deck(self):
-        deck = [
-            ("2 of Hearts", 2),
-            ("3 of Hearts", 3),
-            ("4 of Hearts", 4),
-            ("5 of Hearts", 5),
-            ("6 of Hearts", 6),
-            ("7 of Hearts", 7),
-            ("8 of Hearts", 8),
-            ("9 of Hearts", 9),
-            ("10 of Hearts", 10),
-            ("Jack of Hearts", 10),
-            ("Queen of Hearts", 10),
-            ("King of Hearts", 10),
-            ("Ace of Hearts", 11),
-            ("2 of Diamonds", 2),
-            ("3 of Diamonds", 3),
-            ("4 of Diamonds", 4),
-            ("5 of Diamonds", 5),
-            ("6 of Diamonds", 6),
-            ("7 of Diamonds", 7),
-            ("8 of Diamonds", 8),
-            ("9 of Diamonds", 9),
-            ("10 of Diamonds", 10),
-            ("Jack of Diamonds", 10),
-            ("Queen of Diamonds", 10),
-            ("King of Diamonds", 10),
-            ("Ace of Diamonds", 11),
-            ("2 of Clubs", 2),
-            ("3 of Clubs", 3),
-            ("4 of Clubs", 4),
-            ("5 of Clubs", 5),
-            ("6 of Clubs", 6),
-            ("7 of Clubs", 7),
-            ("8 of Clubs", 8),
-            ("9 of Clubs", 9),
-            ("10 of Clubs", 10),
-            ("Jack of Clubs", 10),
-            ("Queen of Clubs", 10),
-            ("King of Clubs", 10),
-            ("Ace of Clubs", 11),
-            ("2 of Spades", 2),
-            ("3 of Spades", 3),
-            ("4 of Spades", 4),
-            ("5 of Spades", 5),
-            ("6 of Spades", 6),
-            ("7 of Spades", 7),
-            ("8 of Spades", 8),
-            ("9 of Spades", 9),
-            ("10 of Spades", 10),
-            ("Jack of Spades", 10),
-            ("Queen of Spades", 10),
-            ("King of Spades", 10),
-            ("Ace of Spades", 11),
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        names = [
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "Jack",
+            "Queen",
+            "King",
+            "Ace",
         ]
+        values = {
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 10,
+            "Jack": 10,
+            "Queen": 10,
+            "King": 10,
+            "Ace": 11,
+        }
+
+        deck = []
+
+        for suit in suits:
+            for name in names:
+                card_value = values[name]
+                card_name = f"{name} of {suit}"
+                # Assuming you have a consistent naming scheme for your images
+                image_path = f"/cards/card_{suit.lower()}_{name.zfill(2)}.png"
+                deck.append((card_name, card_value, image_path))
         shuffle(deck)
         return deck
 
