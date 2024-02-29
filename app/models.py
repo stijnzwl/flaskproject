9d12118784063cfa4fed508ee383e43ede655e24
stijnzwl <stijnwillems97@hotmail.com>
2024-02-29 21:49:21 +0100

    databasegame

--- app/models.py
+++ app/models.py
@@ -133,3 +133,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
+
+
+
+class Game(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
