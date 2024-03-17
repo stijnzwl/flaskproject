4fabcde3fa290883351f21280e1e464ad1bf53e5
stijnzwl <stijnwillems97@gmail.com>
2024-03-17 14:26:29 +0100

    email sending

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,16 @@
 import time
 from rq import get_current_job
+from app import create_app, db
+from app.models import Task, User, Post
+import sys
+import sqlalchemy as sa
+import json
+from flask import render_template
+from app.email import send_email
+
+
+app = create_app()
+app.app_context().push()
 
 
 def example(seconds):
@@ -13,3 +24,57 @@ def example(seconds):
     job.meta["progress"] = 100
     job.save_meta()
     print("Task completed")
+
+
+def _set_task_progress(progress):
+    job = get_current_job()
+    if job:
+        job.meta["progress"] = progress
+        job.save_meta()
+        task = db.session.get(Task, job.get_id())
+        task.user.add_notification(
+            "task_progress", {"task_id": job.get_id(), "progress": progress}
+        )
+        if progress >= 100:
+            task.complete = True
+        db.session.commit()
+
+
+def export_posts(user_id):
+    try:
+        user = db.session.get(User, user_id)
+        _set_task_progress(0)
+        data = []
+        i = 0
+        total_posts = db.session.scalar(
+            sa.select(sa.func.count()).select_from(user.posts.select().subquery())
+        )
+        for post in db.session.scalars(
+            user.posts.select().order_by(Post.timestamp.asc())
+        ):
+            data.append(
+                {"body": post.body, "timestamp": post.timestamp.isoformat() + "Z"}
+            )
+            time.sleep(5)
+            i += 1
+            _set_task_progress(100 * i // total_posts)
+        send_email(
+            "[Flaskproject] Your blog posts",
+            sender=app.config["ADMINS"],
+            recipients=[user.email],
+            text_body=render_template("email/export_posts.txt", user=user),
+            html_body=render_template("email/export_posts.html", user=user),
+            attachments=[
+                (
+                    "posts.json",
+                    "application/json",
+                    json.dumps({"posts": data}, indent=4),
+                )
+            ],
+            sync=True,
+        )
+    except Exception:
+        _set_task_progress(100)
+        app.logger.error("Unhandled exception", exc_info=sys.exc_info())
+    finally:
+        _set_task_progress(100)
